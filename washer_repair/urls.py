1c9735a4187e3f313d3803b5742c5929259f755e
Sergey <main0serg@gmail.com>
2024-05-28 18:34:38 +0600

    added html and css for header and services blocks

--- washer_repair/urls.py
+++ washer_repair/urls.py
@@ -20,11 +20,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from washer_repair import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("orders.urls", namespace="orders")),
+    path("", views.index, name="index"),
+    path("orders/", include("orders.urls", namespace="orders")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
