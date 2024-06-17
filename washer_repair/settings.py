bd7ed013821f814dba9d8a0e413de7b94a457088
Sergey <main0serg@gmail.com>
2024-06-17 23:38:04 +0600

    added static files for collectstatic

--- washer_repair/settings.py
+++ washer_repair/settings.py
@@ -105,12 +105,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static and media
-STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = (BASE_DIR + "/static/"),
-else:
-    STATIC_ROOT = (BASE_DIR + "/static/"),
-MEDIA_URL = "/media/"
+STATIC_URL = "static/"
+# if DEBUG:
+#     STATICFILES_DIRS = (BASE_DIR + "/static/",)
+# else:
+STATIC_ROOT = (BASE_DIR + "/static/"),
+MEDIA_URL = "media/"
 MEDIA_ROOT = (BASE_DIR + "/media/"),
 
 # Phone number settings
